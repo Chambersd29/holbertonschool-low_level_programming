e16131326dee116bbb57f4e0052bc6e38c975003
Chambersd29 <chambers.git29@gmail.com>
2023-08-18 17:47:07 -0700

    1-isdigitFix4

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
-/*
+/**
  * _isdigit - Tes for numbers
  * @c: character in check
  *
  * Return:  diffent of 1 if is number and 0 otherwise
- */
+ **/
 
 int _isdigit(int c)
 {
 
 	if
-		(c >='0' && c <='9')
+		(c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
-	
 }
