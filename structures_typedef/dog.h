ec79d5685857ebbfe1030101865be8f212367f94
Chambersd29 <chambers.git29@gmail.com>
2023-09-03 22:21:38 -0700

    nuevoDog fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dog - struct that contains the dog's info
  *
@@ -14,6 +19,26 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * struct dog_t - struct for created a dog_t info
+ * @name: dog_t name
+ * @age:dog_t age
+ * @owner: dog_t owner
+ **/
+
+/**
+ * struct dog_t - Structut for created a dog.
+ *@name: Name of dog.
+ *@age: Age of dog.
+ *@owner: Owner of dog.
+ */
+typedef struct dog_t
+{
+        char *name;
+        float age;
+        char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
