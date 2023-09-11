159c1e9fc0b74cfb3c2a72195ebcfaddc436ad0f
Chambersd29 <chambers.git29@gmail.com>
2023-09-10 21:53:58 -0700

    betty fix calc.h 3

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -3,14 +3,14 @@
 
 /**
  * struct op - Struct op
- + @op: the operator
+ * @op: the operatoor
  * @f:the function associated
  **/
 
 typedef struct op
 {
 		char *op;
-			int (*f)(int a, int b);
+		int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
