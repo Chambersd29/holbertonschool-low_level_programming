0077c89f13a1d28b522a094ee328603d672cfdff
Chambersd29 <chambers.git29@gmail.com>
2023-09-10 21:52:12 -0700

    betty fix calc.h 2

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -2,10 +2,10 @@
 #define __cal_h__
 
 /**
- *  * struct op - Struct op
- *   * @op: the operator
- *    * @f:the function associated
- *     */
+ * struct op - Struct op
+ + @op: the operator
+ * @f:the function associated
+ **/
 
 typedef struct op
 {
